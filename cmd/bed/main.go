c1f6aeaa77e70a279e7b80e8fe26188559a8f1c1
Alexander F Rødseth <rodseth@gmail.com>
2018-04-11 10:42:03 +0200

    More like Go, less like C

--- cmd/bed/main.go
+++ cmd/bed/main.go
@@ -1,11 +1,63 @@
 package main
 
-import "os"
+/*
+ * bed
+ *
+ * Version: 0.0.0
+ * Author:  itchyny
+ *
+ */
 
-const name = "bed"
-const version = "v0.0.0"
-const author = "itchyny"
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/itchyny/bed/cmdline"
+	"github.com/itchyny/bed/editor"
+	"github.com/itchyny/bed/tui"
+	"github.com/itchyny/bed/window"
+)
+
+const (
+	name     = "bed"
+	failCode = 1
+)
+
+var (
+	errTooMany = errors.New("too many files")
+	errFail    = errors.New("could not run")
+)
+
+func failed(err error) bool {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", name, err)
+		return true
+	}
+	return false
+}
 
 func main() {
-	os.Exit(run(os.Args))
+	if len(os.Args) > 2 {
+		failed(errTooMany)
+		os.Exit(failCode)
+	}
+
+	editor := editor.NewEditor(tui.NewTui(), window.NewManager(), cmdline.NewCmdline())
+	if failed(editor.Init()) {
+		os.Exit(failCode)
+	}
+	if len(os.Args) > 1 {
+		err := editor.Open(os.Args[1])
+		if failed(err) {
+			os.Exit(failCode)
+		}
+	} else {
+		if failed(editor.OpenEmpty()) {
+			os.Exit(failCode)
+		}
+	}
+	if failed(editor.Run()) || failed(editor.Close()) {
+		os.Exit(failCode)
+	}
 }
